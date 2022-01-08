fbb95092895f0852a2f3502f9c4a4954f07d751b
Grant Elliott <gelliott181@gmail.com>
2022-01-08 12:18:11 -0500

    add api

--- lib/todo-aws-stack.ts
+++ lib/todo-aws-stack.ts
@@ -1,13 +1,24 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+
 import { StaticWebsiteNestedStack } from './stacks/StaticWebsiteStack';
+
 export class TodoAwsStack extends Stack {
+  api: apigateway.RestApi;
   client: StaticWebsiteNestedStack;
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
     this.client = new StaticWebsiteNestedStack(this, 'TodoAppClient');
 
-
+    this.api = new apigateway.RestApi(this, 'TodoRestApi', { });
+    this.api.root.addResource('/todos', {
+      defaultCorsPreflightOptions: {
+        allowOrigins: [ 'https://*.prettycool.link' ],
+        allowMethods: [ 'GET' ]
+      }
+    })
   }
 }
