5ebc61546de95c0fb59deffa1edeb86f48db6def
Grant Elliott <gelliott181@gmail.com>
2022-01-08 12:10:58 -0500

    readme

--- lib/todo-aws-stack.ts
+++ lib/todo-aws-stack.ts
@@ -1,16 +1,13 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
+import { StaticWebsiteNestedStack } from './stacks/StaticWebsiteStack';
 export class TodoAwsStack extends Stack {
+  client: StaticWebsiteNestedStack;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+    this.client = new StaticWebsiteNestedStack(this, 'TodoAppClient');
 
-    // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TodoAwsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
